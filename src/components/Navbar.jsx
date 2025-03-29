f50f72f9ed9d4c2d4307017964032c85d48529ed
Keshaunj <keshaunjones48@gmail.com>
2025-03-28 20:47:48 -0400

    fixed css properties

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,22 +3,24 @@ import { useState } from "react";
 const Navbar = () => {
   return (
     <div>
+    <div>
       <nav className="title-navbar">
+        
         <h1> Your Attire </h1>
+        <ul>
+            <li><a href="#">Women</a></li>
+            <li><a href="#">Men</a></li>
+            <li><a href="#">Beauty</a></li>
+          </ul>
       </nav>
+      </div>
+      <div>
       <nav className="search-navbar">
         <input type="text" placeholder="Search ..." />
         <button> Search </button>
-        <ul>
-          <li>
-            <a href="#">Women</a>
-          </li>
-          <li>
-            <a href="#">Men</a>
-          </li>
-          <li>Beauty</li>
-        </ul>
+     
       </nav>
+    </div>
     </div>
   );
 };
